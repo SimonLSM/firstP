999e5e7a710bd6859034d71bed67065c479e524f
SimonLSM <SimonLeemiao@163.com>
2018-12-22 20:31:30 +0800

    jj

---
+++ demo_02.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-12-22 20:30:00
+# @Author  : Simon Lee (SimonLeemiao@163.com)
+# @Link    : http://www.cnblogs.com/honkly/
+# @Version : $Id$
+
+import os
+
+print("hello world")
